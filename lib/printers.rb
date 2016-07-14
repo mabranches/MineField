0db00c1761a1a95cd9ece6ee0eb8a16d20ae92e9
Manoel Rebelo Abranches <abranches@gmail.com>
2016-07-14 09:53:15 -0300

    to_s in printer class for testing

--- lib/printers.rb
+++ lib/printers.rb
@@ -1,7 +1,13 @@
 class Printer
 
   def initialize(state)
-    @state = state
+    @row = state[:row]
+    @col = state[:col]
+    @bombs = state[:bombs]
+    @not_discovered = state[:not_discovered]
+    @flags = state[:flags]
+    @bombs_vinicity_state = state[:bombs_vinicity_state]
+    @bomb_clicked  = state[:bomb_clicked]
   end
 end
 require './lib/printers/terminal.rb'
